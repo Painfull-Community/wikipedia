75b25a7af0580a718d11f299bbd1ae87d86ac015
Vukky <46850780+Vukkyy@users.noreply.github.com>
2022-05-13 18:40:15 +0000

    lol

--- src/index.js
+++ src/index.js
@@ -11,12 +11,11 @@ module.exports = {
 					const sectionName = new URL(json.request.res.responseUrl).hash;
 					return message.reply({ content: cls.getString("wikipedia", "section").replace("$0", json.data.content_urls.desktop.page + sectionName), allowedMentions: { parse: [] }});
 				}
-				return message.reply({ content: `**${json.data.title}**: ${json.data.extract == "" ? json.data.description == undefined ? `Sorry, but no information about this article is available. You can ask Vukky to add a short description, or read the article at <${json.data.content_urls.desktop.page}>.` : json.data.description + `. For more information, read the article at <${json.data.content_urls.desktop.page}>.` : json.data.extract + ` For more information, read the article at <${json.data.content_urls.desktop.page}>.`}`, allowedMentions: { parse: [] }})
+				return message.reply({ content: `**${json.data.title}**: ${json.data.extract == "" ? json.data.description == undefined ? `Sorry, but no information about this article is available. You can read the article at <${json.data.content_urls.desktop.page}>.` : json.data.description + `. For more information, read the article at <${json.data.content_urls.desktop.page}>.` : json.data.extract + ` For more information, read the article at <${json.data.content_urls.desktop.page}>.`}`, allowedMentions: { parse: [] }})
 			})
 			.catch(function(error) {
-				if(error && error.response && error.response.data && error.response.data.detail) return message.reply({ content: `${args[0] == null ? "" : `**${args.join(" ")}**: `}${error.response.data.detail}\nContact Vukky for more details.`, allowedMentions: { parse: [] }});
-				if(error && error.response && error.response.data && error.response.data.title) return message.reply({ content: `${args[0] == null ? "" : `**${args.join(" ")}**: `}${error.response.data.title}\nContact Vukky for more details.`, allowedMentions: { parse: [] }});
-				if(error && error.response && error.response.status && error.response.status == 404) return message.reply({ content: "The wiki you are trying to access does not support Pukkie.\nContact Vukky for more details."});
+				if(error && error.response && error.response.data && error.response.data.detail) return message.reply({ content: `${args[0] == null ? "" : `**${args.join(" ")}**: `}${error.response.data.detail}`, allowedMentions: { parse: [] }});
+				if(error && error.response && error.response.data && error.response.data.title) return message.reply({ content: `${args[0] == null ? "" : `**${args.join(" ")}**: `}${error.response.data.title}`, allowedMentions: { parse: [] }});
 				if(error) console.error(error);
 				message.reply({ content: cls.getString("core", "error.generic")});
 			});
